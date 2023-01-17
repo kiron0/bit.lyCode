57f5818971978d498bf55a35a4a61a531a51e76e
Toufiq Hasan  Kiron <toufiqhasankiron0@gmail.com>
2023-01-17 20:44:04 +0600

    preloader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Input from './pages/Input';
 import Redirect from './pages/Redirect';
 import NotFound from './shared/NotFound';
+import Preloader from './shared/Preloader/Preloader';
 
 export const InitializeContext = createContext(null as any);
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
   return (
-      <div className="bg-[url('./assets/bg.png')] bg-cover">
-        <Routes>
-          <Route path="/" element={<Input />} />
-          <Route path="/:slug" element={<Redirect />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Toaster />
-      </div>
+    <>
+      {
+        loading ? (
+          <Preloader />
+        ) : (
+          <div className="bg-[url('./assets/bg.png')] bg-cover">
+            <Routes>
+              <Route path="/" element={<Input />} />
+              <Route path="/:slug" element={<Redirect />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Toaster />
+          </div>
+        )
+      }
+    </>
   );
 }
 
